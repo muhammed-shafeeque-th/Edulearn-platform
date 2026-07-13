5019ef45aae04510e9be3c6b02b1a2abed8d2558
shafeeque <shafeeqsha06@gmail.com>
2026-07-13 13:15:59 +0530

    feat(tf-dev): update terraform dev env file to save outputs to localfile

--- infra/terraform/environments/dev/locals.tf
+++ infra/terraform/environments/dev/locals.tf
@@ -6,4 +6,11 @@ locals {
     Environment = local.environment
     ManagedBy   = "Terraform"
   }
+
+  outputs_map = {
+    bastion_public_ip    = module.bastion.bastion_public_ip
+    eks_cluster_endpoint = module.eks.cluster_endpoint
+    cluster_name         = module.eks.cluster_name
+    vpc_id               = module.network.vpc_id
+  }
 }
--- infra/terraform/environments/dev/main.tf
+++ infra/terraform/environments/dev/main.tf
@@ -34,6 +34,66 @@ module "eks" {
   tags               = local.tags
 }
 
+module "aws_lbc_pod_identity" {
+
+  source = "../../modules/pod-identity"
+
+  cluster_name = module.eks.cluster_name
+
+  namespace = "kube-system"
+
+  service_account = "aws-load-balancer-controller"
+
+  role_name = "aws-load-balancer-controller-role"
+
+  policy_name = "AWSLoadBalancerControllerIAMPolicy"
+
+  policy_document = file("${path.module}/policies/aws-lbc-policy.json")
+}
+module "external_secret_pod_identity" {
+
+  source = "../../modules/pod-identity"
+
+  cluster_name = module.eks.cluster_name
+
+  namespace = "external-secrets"
+
+  service_account = "external-secrets"
+
+  role_name = "external-secrets-pod-identity-role"
+
+  policy_name = "ExternalSecretsManagerRead"
+
+  policy_document = file("${path.module}/policies/external-secret-policy.json")
+}
+module "external_dns_pod_identity" {
+
+  source = "../../modules/pod-identity"
+
+  cluster_name = module.eks.cluster_name
+
+  namespace = "external-dns"
+
+  service_account = "external-dns"
+
+  role_name = "external-dns-pod-identity-role"
+
+  policy_name = "AllowExternalDNSUpdates"
+
+  policy_document = file("${path.module}/policies/external-dns-policy.json")
+}
+
+# module "alb" {
+#   source = "../../modules/alb"
+# 
+#   cluster_name      = var.cluster_name
+#   vpc_id            = module.network.vpc_id
+#   region            = var.region
+#   oidc_provider_url = module.eks.oidc_provider_url
+#   tags              = local.tags
+# 
+# }
+
 # Generate a key and registers it in AWS.
 
 resource "tls_private_key" "bastion_key" {
@@ -48,9 +108,14 @@ resource "aws_key_pair" "bastion_keypair" {
 
 
 # Save the private key locally
-
 resource "local_file" "bastion_private_key" {
   content         = tls_private_key.bastion_key.private_key_pem
   filename        = "bastion-key.pem"
   file_permission = "0400"
 }
+
+resource "local_file" "terraform_outputs_yaml" {
+  content  = yamlencode(local.outputs_map)
+  filename = var.outputs_yaml_path
+}
+
