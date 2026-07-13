aad77cf11cc0e94ee2b4ee6eed27cc6aeb2263f2
shafeeque <shafeeqsha06@gmail.com>
2026-07-13 13:00:22 +0530

    feat(tf-versions): add terraform versions file

--- infra/terraform/versions.tf
+++ infra/terraform/versions.tf
@@ -8,10 +8,5 @@ terraform {
       version = "~> 6.53"     # Allows 6.x and above
     }
 
-    # Add more providers here in future (e.g., kubernetes, helm, etc.)
-    # kubernetes = {
-    #   source  = "hashicorp/kubernetes"
-    #   version = "~> 2.30"
-    # }
   }
 }
