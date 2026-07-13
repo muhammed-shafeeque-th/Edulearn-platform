172904f7606b456a50db93f47b758e8a59292157
shafeeque <shafeeqsha06@gmail.com>
2026-07-13 13:10:48 +0530

    feat(tf-eks): update terraform eks module's output file to output oicd provider url

--- infra/terraform/modules/eks/output.tf
+++ infra/terraform/modules/eks/output.tf
@@ -14,3 +14,8 @@ output "cluster_security_group_id" {
 output "oidc_provider_arn" {
   value = module.eks.oidc_provider_arn
 }
+output "oidc_provider_url" {
+  # provider_url = replace(module.eks..identity[0].oidc[0].issuer, "https://", "")
+  value = module.eks.cluster_oidc_issuer_url
+}
+
